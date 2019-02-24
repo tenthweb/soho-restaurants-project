4b6695830bd9cbf0090e596b6a9e6585330d78fe
tenthweb <47951574+tenthweb@users.noreply.github.com>
2019-02-24 21:30:29 +0000

    Delete hashtester.py

--- hashtester.py
+++
@@ -1,7 +0,0 @@
-from hashmap import *
-
-types = ['arcoxia 30', 'janumet','azilect', 'arcoxia 90', 'januvia', 'run', 'runners', 'runner', 'running','germane', 'ventolin']
-
-
-restaurants = HashMap(len(types))
-
